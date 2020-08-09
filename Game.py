019fac72f469c2f6fb6a9d8fa1e687db4afb6971
Abhijat Vatsyayan <abhijat.vatsyayan@gmail.com>
2020-08-09 17:53:54 -0400

    Adding gaming harness

---
+++ Game.py
@@ -0,0 +1,47 @@
+import pgzrun
+
+import GameObjects as go
+
+WIDTH = 1200
+HEIGHT = 900
+
+
+class ActorMaker:
+
+    def make_actor(self, img_name):
+        return Actor(img_name)
+
+
+actor_maker = ActorMaker()
+world = go.World(actor_maker, WIDTH, HEIGHT)
+
+space_ship = world.make_game_actor("space_ship_005")
+center_x, center_y = world.get_center()
+space_ship.set_location(center_x, HEIGHT - 50)
+meteor = world.make_game_actor("space_meteor_001")
+world.add_actor(space_ship)
+world.add_actor(meteor)
+
+ship_mover = go.CanMoveLeftRight(space_ship, keys.LEFT, keys.RIGHT)
+can_fire = go.CanFire(space_ship,  keys.SPACE)
+world.add_key_handler(ship_mover)
+world.add_key_handler(can_fire)
+
+meteor_updater = go.UpDownPath(meteor, 300, 0, True)
+world.add_location_updater(meteor_updater)
+
+
+def draw():
+    screen.clear()
+    world.draw()
+
+
+def update():
+    world.update()
+
+
+def on_key_down(key, mod):
+    world.on_key_down(key, mod)
+
+
+pgzrun.go()
---
+++ README.md
@@ -0,0 +1,15 @@
+## TODO 
+
+* Provide an  option in the UpDownPath to loop (or not to loop)
+* Make the ship move as long as left or right key is pressed
+* Make a separate meteor class, then make it change its image after every "n" updates 
+* Make the meteor smaller
+* Detect when meteor hits either the missile or the ship 
+* Destroy a meteor (remove from world as well) if missile hits it
+* Destropy the ship if meteor hits it 
+* Initialize with different number of meteors , and speed. Might be fun to find 
+different kinds of images for this.
+* Put restrictions on how my misslies can be fired at any given time - may be a counter for delay.
+* Add shield etc. you can keep on adding more stuff to game play 
+* Experiment with different kind of paths 
+
